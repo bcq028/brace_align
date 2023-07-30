e6de1a22f0c9fee8ab596458ea7954e681a15221
bcq028 <108849949+bcq028@users.noreply.github.com>
2023-07-30 17:57:32 +0800

    webpack

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('brace-align.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from brace_align!');
+		vscode.window.showInformationMessage('Hello World from brace-align!');
 	});
 
 	context.subscriptions.push(disposable);
